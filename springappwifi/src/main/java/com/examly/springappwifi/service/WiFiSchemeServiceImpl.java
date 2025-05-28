db8c58b5e5ea1877ec04ec98e3361b4b714fb497
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 18:20:55 +0530

    Update WiFiSchemeServiceImpl.java

--- springappwifi/src/main/java/com/examly/springappwifi/service/WiFiSchemeServiceImpl.java
+++ springappwifi/src/main/java/com/examly/springappwifi/service/WiFiSchemeServiceImpl.java
@@ -1,35 +1,74 @@
 package com.examly.springappwifi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.examly.springappwifi.model.User;
 import com.examly.springappwifi.model.WiFiSchema;
-import com.examly.springappwifi.repository.WiFiSchemaRepo;
+import com.examly.springappwifi.model.WiFiSchemaRequest;
+import com.examly.springappwifi.repository.UserRepo;
+import com.examly.springappwifi.repository.WiFiSchemaRequestRepo;
 
 @Service
-public class WiFiSchemaServiceImpl implements WiFiSchemaService {
+public class WiFiSchemsRequestServiceImpl implements WiFiSchemaRequestService {
 
-    private final WiFiSchemaRepo wifiSchemaRepo;
+    private final WiFiSchemaRequestRepo wifiSchemaRequestRepo;
+    private final UserRepo userRepo;
+    private final WiFiSchemaService wifiSchemaService;
 
-    public WiFiSchemaServiceImpl(WiFiSchemaRepo wifiSchemaRepo) {
-        this.wifiSchemaRepo = wifiSchemaRepo;
+    public WiFiSchemsRequestServiceImpl(WiFiSchemaRequestRepo wifiSchemaRequestRepo,
+            UserRepo userRepo,
+            WiFiSchemaService wifiSchemaService) {
+        this.wifiSchemaRequestRepo = wifiSchemaRequestRepo;
+        this.userRepo = userRepo;
+        this.wifiSchemaService = wifiSchemaService;
     }
 
     @Override
-    public WiFiSchema createWiFiSchema(WiFiSchema wifiSchema) {
-        return wifiSchemaRepo.save(wifiSchema);
+    public WiFiSchemaRequest createWiFiSchemaRequest(WiFiSchemaRequest wifiSchemaRequest) {
+
+        if (wifiSchemaRequest.getWifiScheme() == null || wifiSchemaRequest.getWifiScheme().getWifiSchemeId() == 0) {
+            throw new IllegalArgumentException("WiFi Scheme ID is required in the request");
+        }
+
+        // get user by ID or else create a new user
+        Optional<User> user = userRepo.findById(wifiSchemaRequest.getUser().getUserId());
+        if (user.isPresent()) {
+            wifiSchemaRequest.setUser(user.get());
+        } else {
+            wifiSchemaRequest.setUser(userRepo.save(wifiSchemaRequest.getUser()));
+        }
+
+        // get WiFi schema by ID
+        WiFiSchema wiFiSchema = wifiSchemaService
+                .getWiFiSchemaById(wifiSchemaRequest.getWifiScheme().getWifiSchemeId());
+        if (wiFiSchema != null) {
+            wifiSchemaRequest.setWifiScheme(wiFiSchema);
+        } else {
+            throw new IllegalArgumentException(
+                    "WiFi Schema not found for ID: " + wifiSchemaRequest.getWifiScheme().getWifiSchemeId());
+        }
+        return wifiSchemaRequestRepo.save(wifiSchemaRequest);
     }
 
     @Override
-    public WiFiSchema getWiFiSchemaById(long wifiSchemeId) {
-        return wifiSchemaRepo.findById(wifiSchemeId)
-                .orElseThrow(() -> new RuntimeException("WiFi Schema not found with id: " + wifiSchemeId));
+    public WiFiSchemaRequest getWiFiSchemaRequestById(long wifiSchemeRequestId) {
+        return wifiSchemaRequestRepo.findById(wifiSchemeRequestId)
+                .orElseThrow(() -> new RuntimeException("WiFi Schema Request not found with id: " + wifiSchemeRequestId));
     }
 
     @Override
-    public List<WiFiSchema> getAllWiFiSchemas() {
-        return wifiSchemaRepo.findAll();
+    public List<WiFiSchemaRequest> getWiFiSchemasByUserId(long userId) {
+       
+       return  wifiSchemaRequestRepo.findByUser_UserId(userId);
+        
+    }
+
+    @Override
+    public List<WiFiSchemaRequest> getAllWiFiSchemaRequests() {
+        return wifiSchemaRequestRepo.findAll();
     }
 
 }
