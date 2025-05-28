d76e0eaac6a2aa43ea473294262b8397b4e5dbe9
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 18:06:32 +0530

    Update AuthController.java

--- springappuser/src/main/java/com/examly/springappuser/controller/AuthController.java
+++ springappuser/src/main/java/com/examly/springappuser/controller/AuthController.java
@@ -1,47 +1,37 @@
 package com.examly.springappuser.controller;
 
+import java.net.URI;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.examly.springappuser.config.JwtUtils;
 import com.examly.springappuser.model.LoginDTO;
 import com.examly.springappuser.model.User;
 import com.examly.springappuser.service.UserService;
 
+
 @RestController
-@RequestMapping("/api")
 public class AuthController {
-    private UserService userService;
-    private JwtUtils jwtUtils;
-    @Autowired
-    public AuthController(UserService userService, JwtUtils jwtUtils) {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
         this.userService = userService;
-        this.jwtUtils = jwtUtils;
     }
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody User user){
-        User savedUser = userService.createUser(user);
-        System.out.println(savedUser.getUserRole());
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+
+    @PostMapping("/api/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+
+        String userResponse = userService.createUser(user);
+
+        return ResponseEntity.created(URI.create("/api/user"))
+                .body(userResponse);
     }
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody LoginDTO request){
-        User user = userService.loginUser(request.getEmail(), request.getPassword());
-        String token = jwtUtils.generateToken(user.getEmail());
-        System.out.println("------------------------------------------------");
-        System.out.println("------------------------------------------------");
-        System.out.println("------------------------------------------------");
-        System.out.println("Token -----------------------"+token);
-        System.out.println("------------------------------------------------");
-        System.out.println("------------------------------------------------");
-        System.out.println("------------------------------------------------");
-        return ResponseEntity.ok(new LoginResponse(token));
+
+    @PostMapping("/api/login")
+    public ResponseEntity<LoginDTO> loginUser(@RequestBody User user) {
+
+        return ResponseEntity.ok(userService.loginUser(user));
     }
-    
+
 }
