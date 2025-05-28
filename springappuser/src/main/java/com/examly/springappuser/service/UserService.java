5bdcc0b0859c5a265afb6f26c96258a4f8012c22
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 18:08:23 +0530

    Update UserService.java

--- springappuser/src/main/java/com/examly/springappuser/service/UserService.java
+++ springappuser/src/main/java/com/examly/springappuser/service/UserService.java
@@ -1,8 +1,15 @@
 package com.examly.springappuser.service;
 
+import org.springframework.stereotype.Service;
+
+import com.examly.springappuser.model.LoginDTO;
 import com.examly.springappuser.model.User;
 
+@Service
 public interface UserService {
-    User createUser(User user);
-    User loginUser(String email,String password);
+
+    String createUser(User user);
+
+    LoginDTO loginUser(User user);
+    
 }
