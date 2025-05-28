01bbc19e0811d70a83b3b7207847b67385aa49ec
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 18:07:33 +0530

    Update LoginDTO.java

--- springappuser/src/main/java/com/examly/springappuser/model/LoginDTO.java
+++ springappuser/src/main/java/com/examly/springappuser/model/LoginDTO.java
@@ -1,21 +1,55 @@
 package com.examly.springappuser.model;
 
-
-
 public class LoginDTO {
-    private String email;
-    private String password;
-    public String getEmail() {
-        return email;
+
+    // token,username,userRole,userId
+
+    private String token;
+    private String username;
+    private String userRole;
+    private int userId;
+
+    public LoginDTO() {
     }
-    public void setEmail(String email) {
-        this.email = email;
+
+    public LoginDTO(String token, String username, String userRole, int userId) {
+        this.token = token;
+        this.username = username;
+        this.userRole = userRole;
+        this.userId = userId;
     }
-    public String getPassword() {
-        return password;
+
+    public String getToken() {
+        return token;
     }
-    public void setPassword(String password) {
-        this.password = password;
+
+    public void setToken(String token) {
+        this.token = token;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    
     
 }
