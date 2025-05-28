db608fb511cbe673f1e039a71692e96f42ee59b4
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 17:58:41 +0530

    Update Feedback.java

--- springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
+++ springappfeedback/src/main/java/com/examly/springappfeedback/model/Feedback.java
@@ -2,64 +2,88 @@ package com.examly.springappfeedback.model;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
-@Table(name = "feedbacks")
 public class Feedback {
+
+    // feedbackId, feedbackText, date, User, wifiSchema, category
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long feedbackId;
-    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long feedbackId;
     private String feedbackText;
-    @Column(nullable = false)
     private LocalDate date;
-    @Column(nullable = false)
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "wifiSchemeId", nullable = false)
+    @JsonProperty("wifiScheme")
+    private WiFiSchema wifiSchema;
     private String category;
-    @Column(nullable = false)
-    private Long userId;
-    @Column(nullable = false)
-    private Long wifiSchemeId;
-    public Long getFeedbackId() {
+
+    public Feedback() {
+        // no-arg constructor
+    }
+
+    public long getFeedbackId() {
         return feedbackId;
     }
-    public void setFeedbackId(Long feedbackId) {
+
+    public void setFeedbackId(long feedbackId) {
         this.feedbackId = feedbackId;
     }
+
     public String getFeedbackText() {
         return feedbackText;
     }
+
     public void setFeedbackText(String feedbackText) {
         this.feedbackText = feedbackText;
     }
+
     public LocalDate getDate() {
         return date;
     }
+
     public void setDate(LocalDate date) {
         this.date = date;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public WiFiSchema getWifiSchema() {
+        return wifiSchema;
+    }
+
+    public void setWifiSchema(WiFiSchema wifiSchema) {
+        this.wifiSchema = wifiSchema;
+    }
+
     public String getCategory() {
         return category;
     }
+
     public void setCategory(String category) {
         this.category = category;
     }
-    public Long getUserId() {
-        return userId;
-    }
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-    public Long getWifiSchemeId() {
-        return wifiSchemeId;
-    }
-    public void setWifiSchemeId(Long wifiSchemeId) {
-        this.wifiSchemeId = wifiSchemeId;
-    }
+
     
+
 }
