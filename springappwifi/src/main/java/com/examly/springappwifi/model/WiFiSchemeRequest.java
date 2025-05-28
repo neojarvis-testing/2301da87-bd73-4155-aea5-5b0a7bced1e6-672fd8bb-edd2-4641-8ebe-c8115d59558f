d248040f235e0ec231b4b400b27757724a0df3cc
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 18:18:08 +0530

    Update WiFiSchemeRequest.java

--- springappwifi/src/main/java/com/examly/springappwifi/model/WiFiSchemeRequest.java
+++ springappwifi/src/main/java/com/examly/springappwifi/model/WiFiSchemeRequest.java
@@ -2,6 +2,8 @@ package com.examly.springappwifi.model;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,20 +13,25 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class WiFiSchemeRequest {
+public class WiFiSchemaRequest {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long wifiSchemeRequestId;
+    private long wiFiSchemaRequestId;
+
     @ManyToOne
-    @JoinColumn(name = "userId")
+    @JoinColumn(name = "userId", nullable = false)
     private User user;
+
     @ManyToOne
-    @JoinColumn(name="wifiSchemeId")
-    private WiFiScheme wiFiScheme;
-    private LocalDate requesDate;
+    @JoinColumn(name = "wifiSchemeId", nullable = false)
+    @JsonProperty("wifiScheme")
+    private WiFiSchema wifiScheme;
+    private LocalDate requestDate;
     private String status;
     private String comments;
 
+    // lob base 64 encoded string
     @Lob
     private String proof;
     private String streetName;
@@ -32,157 +39,132 @@ public class WiFiSchemeRequest {
     private String city;
     private String state;
     private String zipCode;
-    private LocalDate preferredSetupDate;
+    private LocalDate prefferedSetupDate;
+    private String prefferedSetupTime;
     private String timeSlot;
-    
-    public Long getWifiSchemeRequestId() {
-        return wifiSchemeRequestId;
+
+    public WiFiSchemaRequest() {
+        // Default constructor
     }
 
-
-    public void setWifiSchemeRequestId(Long wifiSchemeRequestId) {
-        this.wifiSchemeRequestId = wifiSchemeRequestId;
+    public long getWiFiSchemaRequestId() {
+        return wiFiSchemaRequestId;
     }
 
+    public void setWiFiSchemaRequestId(long wiFiSchemaRequestId) {
+        this.wiFiSchemaRequestId = wiFiSchemaRequestId;
+    }
 
     public User getUser() {
         return user;
     }
 
-
     public void setUser(User user) {
         this.user = user;
     }
 
-
-    public WiFiScheme getWiFiScheme() {
-        return wiFiScheme;
+    public WiFiSchema getWifiScheme() {
+        return wifiScheme;
     }
 
-
-    public void setWiFiScheme(WiFiScheme wiFiScheme) {
-        this.wiFiScheme = wiFiScheme;
+    public void setWifiScheme(WiFiSchema wifiScheme) {
+        this.wifiScheme = wifiScheme;
     }
 
-
-    public LocalDate getRequesDate() {
-        return requesDate;
+    public LocalDate getRequestDate() {
+        return requestDate;
     }
 
-
-    public void setRequesDate(LocalDate requesDate) {
-        this.requesDate = requesDate;
+    public void setRequestDate(LocalDate requestDate) {
+        this.requestDate = requestDate;
     }
-
 
     public String getStatus() {
         return status;
     }
 
-
     public void setStatus(String status) {
         this.status = status;
     }
-
 
     public String getComments() {
         return comments;
     }
 
-
     public void setComments(String comments) {
         this.comments = comments;
     }
-
 
     public String getProof() {
         return proof;
     }
 
-
     public void setProof(String proof) {
         this.proof = proof;
     }
-
 
     public String getStreetName() {
         return streetName;
     }
 
-
     public void setStreetName(String streetName) {
         this.streetName = streetName;
     }
-
 
     public String getLandmark() {
         return landmark;
     }
 
-
     public void setLandmark(String landmark) {
         this.landmark = landmark;
     }
-
 
     public String getCity() {
         return city;
     }
 
-
     public void setCity(String city) {
         this.city = city;
     }
-
 
     public String getState() {
         return state;
     }
 
-
     public void setState(String state) {
         this.state = state;
     }
-
 
     public String getZipCode() {
         return zipCode;
     }
 
-
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
-
-    public LocalDate getPreferredSetupDate() {
-        return preferredSetupDate;
+    public LocalDate getPrefferedSetupDate() {
+        return prefferedSetupDate;
     }
 
-
-    public void setPreferredSetupDate(LocalDate preferredSetupDate) {
-        this.preferredSetupDate = preferredSetupDate;
+    public void setPrefferedSetupDate(LocalDate prefferedSetupDate) {
+        this.prefferedSetupDate = prefferedSetupDate;
     }
 
+    public String getPrefferedSetupTime() {
+        return prefferedSetupTime;
+    }
+
+    public void setPrefferedSetupTime(String prefferedSetupTime) {
+        this.prefferedSetupTime = prefferedSetupTime;
+    }
 
     public String getTimeSlot() {
         return timeSlot;
     }
 
-
     public void setTimeSlot(String timeSlot) {
         this.timeSlot = timeSlot;
     }
 
-
-    @Override
-    public String toString() {
-        return "WiFiSchemeRequest [wifiSchemeRequestId=" + wifiSchemeRequestId + ", user=" + user + ", wiFiScheme="
-                + wiFiScheme + ", requesDate=" + requesDate + ", status=" + status + ", comments=" + comments
-                + ", proof=" + proof + ", streetName=" + streetName + ", landmark=" + landmark + ", city=" + city
-                + ", state=" + state + ", zipCode=" + zipCode + ", preferredSetupDate=" + preferredSetupDate
-                + ", timeSlot=" + timeSlot + "]";
-    }
-
-    
 }
