3fbb67bec2bd834e9ad0ce798f5f0986d338be9c
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 18:16:58 +0530

    Update WiFiSchemeController.java

--- springappwifi/src/main/java/com/examly/springappwifi/controller/WiFiSchemeController.java
+++ springappwifi/src/main/java/com/examly/springappwifi/controller/WiFiSchemeController.java
@@ -1,12 +1,10 @@
 package com.examly.springappwifi.controller;
 
+import java.net.URI;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,38 +13,55 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.examly.springappwifi.model.WiFiScheme;
-import com.examly.springappwifi.service.WiFiSchemeService;
+import com.examly.springappwifi.model.WiFiSchema;
+import com.examly.springappwifi.service.WiFiSchemaService;
 
 @RestController
-@RequestMapping("/api/wifischemes")
-@CrossOrigin
-public class WiFiSchemeController {
-    @Autowired
-    private WiFiSchemeService schemeService;
-    @PostMapping
-    // @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<WiFiScheme> addWiFiScheme(@RequestBody WiFiScheme scheme){
-        return new ResponseEntity<>(schemeService.addWiFiScheme(scheme),HttpStatus.CREATED);
-    }
-    @GetMapping
-    public ResponseEntity<List<WiFiScheme>> getAllWiFiScheme(){
-        return ResponseEntity.ok(schemeService.getAllWiFiSchemes());
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity<WiFiScheme> getWiFiSchemeById(@PathVariable Long id){
-        return schemeService.getWiFiSchemeById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-    }
-    @PutMapping("/{id}")
-    // @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<WiFiScheme> updateWiFiScheme(@PathVariable Long id, @RequestBody WiFiScheme scheme){
-        return ResponseEntity.ok(schemeService.updateWiFiScheme(id, scheme));
-    }
-    @DeleteMapping("/{id}")
-    // @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Void> deleteWiFiScheme(@PathVariable Long id){
-        schemeService.deleteWiFiScheme(id);
-        return ResponseEntity.noContent().build();
+@RequestMapping("/api/wifiScheme")
+public class WiFiSchemaController {
+
+    private final WiFiSchemaService wifiSchemaService;
+
+    public WiFiSchemaController(WiFiSchemaService wifiSchemaService) {
+        this.wifiSchemaService = wifiSchemaService;
     }
 
+    @PreAuthorize("hasRole('Admin')")
+    @PostMapping
+    public ResponseEntity<WiFiSchema> createWiFiSchema(@RequestBody WiFiSchema wifiSchema) {
+        WiFiSchema createdSchema = wifiSchemaService.createWiFiSchema(wifiSchema);
+        return ResponseEntity.created(URI.create("/api/wifiSchema/" + createdSchema.getWifiSchemeId()))
+                .body(createdSchema);
+    }
+    
+    @PreAuthorize("hasRole('Admin')")
+    @GetMapping("/{wifiSchemeId}")
+    public ResponseEntity<WiFiSchema> getWiFiSchema(@PathVariable long wifiSchemeId) {
+        WiFiSchema wifiSchema = wifiSchemaService.getWiFiSchemaById(wifiSchemeId);
+        return ResponseEntity.ok(wifiSchema);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WiFiSchema>> getAllWiFiSchemas() {
+        List<WiFiSchema> wifiSchemas = wifiSchemaService.getAllWiFiSchemas();
+        return ResponseEntity.ok(wifiSchemas);
+    }
+
+    @PreAuthorize("hasRole('Admin')")
+    @PutMapping("/{wifiSchemeId}")
+    public ResponseEntity<WiFiSchema> updateWiFiSchema(@PathVariable long wifiSchemeId,
+            @RequestBody WiFiSchema wifiSchema) {
+        WiFiSchema existingSchema = wifiSchemaService.getWiFiSchemaById(wifiSchemeId);
+        existingSchema.setSchemeName(wifiSchema.getSchemeName());
+        existingSchema.setDescription(wifiSchema.getDescription());
+        existingSchema.setSpeed(wifiSchema.getSpeed());
+        existingSchema.setDataLimit(wifiSchema.getDataLimit());
+        existingSchema.setFee(wifiSchema.getFee());
+        existingSchema.setAvailabilityStatus(wifiSchema.getAvailabilityStatus());
+
+        WiFiSchema updatedSchema = wifiSchemaService.createWiFiSchema(existingSchema);
+        return ResponseEntity.ok(updatedSchema);
+    }
+
+    
 }
