423fa7eaf303c32e97f02543ac9b72b02e47e7e4
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2025-05-28 18:06:11 +0530

    Update UserPrinciple.java

--- springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
+++ springappuser/src/main/java/com/examly/springappuser/config/UserPrinciple.java
@@ -3,69 +3,104 @@ package com.examly.springappuser.config;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.examly.springappuser.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class UserPrinciple implements UserDetails {
-    private static final long serialVersionUID=1L;
-    private transient User user;
-    public UserPrinciple(User user){
-        this.user=Objects.requireNonNull(user,"User cannot be null");
+
+    private static final long serialVersionUID = 1L;
+
+    private int userId;
+
+    private String username;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+  public UserPrinciple(int userId, String username, String email, String password,
+      Collection<? extends GrantedAuthority> authorities) {
+    this.userId = userId;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.authorities = authorities;
+  }
+
+    public static UserPrinciple build(User user) {
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getUserRole()));
+               
+        return new UserPrinciple(
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
     }
+
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities(){
-        return List.of(new SimpleGrantedAuthority("ROLE_"+user.getUserRole()));
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
     }
-    @Override
-    public String getPassword(){
-        return user.getPassword();
+
+    public int getUserId() {
+        return userId;
     }
-    @Override
-    public String getUsername(){
-        return user.getEmail();
+
+    public String getEmail() {
+        return email;
     }
+
     @Override
-    public boolean isAccountNonLocked(){
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
         return true;
     }
+
     @Override
-    public boolean isAccountNonExpired(){
+    public boolean isAccountNonLocked() {
         return true;
     }
+
     @Override
-    public boolean isCredentialsNonExpired(){
+    public boolean isCredentialsNonExpired() {
         return true;
     }
+
     @Override
-    public boolean isEnabled(){
+    public boolean isEnabled() {
         return true;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserPrinciple user = (UserPrinciple) o;
+        return Objects.equals(userId, user.userId);
+    }
+
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((user == null) ? 0 : user.hashCode());
-        return result;
+        return Objects.hash(userId);
     }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        UserPrinciple other = (UserPrinciple) obj;
-        if (user == null) {
-            if (other.user != null)
-                return false;
-        } else if (!user.equals(other.user))
-            return false;
-        return true;
-    }
-    
+
 }
